12411be3e51e2fd9f80abf37ee9d636286f83725
choibora <97choibora@gmail.com>
2026-07-28 20:33:05 +0900

    Update 260728

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# lab-02
+## Project lab 02
+
+![Day](https://img.shields.io/badge/Day-260728-233D4D?labelColor=464858)
+
+```
+- counter
+
+- splash
+    - useSplashScreen.tsx
+    - app.tsx
+    - SplashScreen.tsx
+```
+
+---
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import SplashScreen from '@/features/splash/SplashScreen';
+import useSplashScreen from '@/features/splash/useSplashScreen';
+
+import './App.css';
 function App() {
+    const { showSplashScreen, splashScreenPhase } = useSplashScreen();
     return (
         <>
+            {showSplashScreen && <SplashScreen phase={splashScreenPhase} />}
             <main>
                 <h1>Get started</h1>
             </main>
